ceb3e1fe4a2d445fa742e17bf48b7e3f6551cfe6
tintinnabulate <tintinn@bulate>
2025-02-15 18:15:16 +0000

    Tweak keymap and macros

--- keyboards/dcpedit/pillzmod/keymaps/tintinnabulate/keymap.c
+++ keyboards/dcpedit/pillzmod/keymaps/tintinnabulate/keymap.c
@@ -3,6 +3,9 @@
 #    include "keymap.h"
 #endif
 
+#if __has_include("sendstring_uk.h")
+#    include "sendstring_uk.h"
+#endif
 
 /* THIS FILE WAS GENERATED!
  *
@@ -12,6 +15,7 @@
 
 enum custom_keycodes {
     QMKBEST = SAFE_RANGE,
+    QMKTWOS,
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -19,9 +23,17 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case QMKBEST:
         if (record->event.pressed) {
             // when keycode QMKBEST is pressed
-            SEND_STRING("Hello, world!");
+            SEND_STRING("Hello, ");
         } else {
             // when keycode QMKBEST is released
+        }
+        break;
+    case QMKTWOS:
+        if (record->event.pressed) {
+            // when keycode QMKTWOS is pressed
+            SEND_STRING("world!");
+        } else {
+            // when keycode QMKTWOS is released
         }
         break;
     }
@@ -29,8 +41,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(KC_DEL, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_PSCR, KC_SCRL, KC_PAUS, TG(1), MO(1), KC_EQL, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS, KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_GRV, KC_INS, KC_LEFT, KC_RGHT, KC_UP, KC_DOWN, KC_LBRC, KC_RBRC, KC_LCTL, KC_LALT, KC_RGUI, KC_RCTL, KC_HOME, KC_PGUP, KC_BSPC, KC_ESC, KC_END, KC_PGDN, KC_ENTER, KC_SPC, KC_A, KC_B, KC_C),
-    [1] = LAYOUT(QK_RBT, QK_BOOT, _______, _______, _______, _______, _______, _______, _______, AU_TOGG, HF_TOGG, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_NUM, KC_PEQL, KC_PSLS, KC_PAST, _______, _______, QMKBEST, _______, _______, _______, _______, _______, KC_P7, KC_P8, KC_P9, KC_PMNS, _______, _______, _______, _______, _______, _______, _______, _______, KC_P4, KC_P5, KC_P6, KC_PPLS, _______, _______, _______, _______, _______, _______, _______, _______, KC_P1, KC_P2, KC_P3, KC_PENT, _______, _______, _______, _______, _______, KC_UP, KC_DOWN, KC_PDOT, KC_PENT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_P0, _______, _______, _______)
+    [0] = LAYOUT(KC_DEL, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_PSCR, KC_SCRL, KC_PAUS, TG(1), MO(1), KC_EQL, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_NUHS, KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_GRV, KC_NUBS, KC_LEFT, KC_RGHT, KC_UP, KC_DOWN, KC_LBRC, KC_RBRC, KC_LCTL, KC_LALT, KC_RGUI, KC_RCTL, KC_HOME, KC_PGUP, KC_BSPC, KC_ESC, KC_END, KC_PGDN, KC_ENTER, KC_SPC, KC_A, KC_B, KC_C),
+    [1] = LAYOUT(QK_RBT, QK_BOOT, _______, _______, _______, _______, _______, _______, _______, AU_TOGG, HF_TOGG, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_NUM, KC_PEQL, KC_PSLS, KC_PAST, _______, _______, QMKBEST, QMKTWOS, _______, _______, _______, _______, KC_P7, KC_P8, KC_P9, KC_PMNS, _______, _______, _______, _______, _______, _______, _______, _______, KC_P4, KC_P5, KC_P6, KC_PPLS, _______, _______, _______, _______, _______, _______, _______, _______, KC_P1, KC_P2, KC_P3, KC_PENT, _______, _______, _______, _______, _______, KC_UP, KC_DOWN, KC_PDOT, KC_PENT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_P0, _______, _______, _______)
 };
 
 
@@ -38,5 +50,4 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #ifdef OTHER_KEYMAP_C
 #    include OTHER_KEYMAP_C
 #endif // OTHER_KEYMAP_C
-
 
