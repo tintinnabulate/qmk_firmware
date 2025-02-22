44e05df5b63ad42f0ab41085453eceba0493c401
tintinnabulate <tintinn@bulate>
2025-02-22 16:23:18 +0000

    Use one-shot layer instead of toggle.
    Fix Fn misalignment of Fn6->

--- keyboards/dcpedit/pillzmod/keymaps/tintinnabulate/keymap.c
+++ keyboards/dcpedit/pillzmod/keymaps/tintinnabulate/keymap.c
@@ -8,26 +8,26 @@
 #endif
 
 enum custom_keycodes {
-    QMKBEST = SAFE_RANGE,
-    QMKTWOS,
+    QMK_Q = SAFE_RANGE,
+    QMK_W,
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-    case QMKBEST:
+    case QMK_Q:
         if (record->event.pressed) {
-            // when keycode QMKBEST is pressed
+            // when keycode QMK_Q is pressed
             SEND_STRING("Hello, ");
         } else {
-            // when keycode QMKBEST is released
+            // when keycode QMK_Q is released
         }
         break;
-    case QMKTWOS:
+    case QMK_W:
         if (record->event.pressed) {
-            // when keycode QMKTWOS is pressed
+            // when keycode QMK_W is pressed
             SEND_STRING("world!");
         } else {
-            // when keycode QMKTWOS is released
+            // when keycode QMK_W is released
         }
         break;
     }
@@ -40,13 +40,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
  * edit it directly.
  */
 
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(KC_DEL, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_PSCR, KC_SCRL, KC_PAUS, KC_RGUI, MO(1), KC_EQL, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_NUHS, KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_GRV, KC_NUBS, KC_LEFT, KC_RGHT, KC_UP, KC_DOWN, KC_LBRC, KC_RBRC, KC_LCTL, KC_LALT, TG(1), KC_RCTL, KC_HOME, KC_PGUP, KC_BSPC, KC_ESC, KC_END, KC_PGDN, KC_ENTER, KC_SPC, KC_A, KC_B, KC_C),
-    [1] = LAYOUT(_______, _______, _______, _______, _______, _______, _______, _______, _______, AU_TOGG, HF_TOGG, _______, _______, _______, _______, _______, _______, _______, _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, _______, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, _______, QMKBEST, QMKTWOS, _______, QK_RBT, _______, _______, _______, _______, _______, QK_BOOT, KC_F11, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_F12, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______)
+    [0] = LAYOUT(KC_DEL, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_PSCR, KC_SCRL, KC_PAUS, KC_RGUI, MO(1), KC_EQL, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_NUHS, KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_GRV, KC_NUBS, KC_LEFT, KC_RGHT, KC_UP, KC_DOWN, KC_LBRC, KC_RBRC, KC_LCTL, KC_LALT, OSL(1), KC_RCTL, KC_HOME, KC_PGUP, KC_BSPC, KC_ESC, KC_END, KC_PGDN, KC_ENTER, KC_SPC, KC_A, KC_B, KC_C),
+    [1] = LAYOUT(_______, _______, _______, _______, _______, _______, _______, _______, _______, AU_TOGG, HF_TOGG, _______, _______, _______, _______, _______, _______, _______, _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, _______, QMK_Q, QMK_W, _______, QK_RBT, _______, _______, _______, _______, _______, QK_BOOT, KC_F12, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______)
 };
+
 
 
 #ifdef OTHER_KEYMAP_C
 #    include OTHER_KEYMAP_C
 #endif // OTHER_KEYMAP_C
+
+
